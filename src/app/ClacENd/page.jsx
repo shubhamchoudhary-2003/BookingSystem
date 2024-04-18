101cdf2243060857b41f5979646e9abc8234f177
shubham <shubhamera484@gmail.com>
2024-04-18 14:34:41 +0530

    first commit

--- src/app/ClacENd/page.jsx
+++ src/app/ClacENd/page.jsx
@@ -1,23 +1,24 @@
-"use client"
-import React from "react";
+"use client";import React, { useState } from "react";
 import { DayPicker } from "react-day-picker";
 
 const ClacENd = () => {
+  const [startDate, setStartDate] = useState(new Date()); // Initialize startDate to current date
   const bookings = [
-    { date: "2024 -04-15", bookNO: 10 },
-    { date: "2024 -04-15", bookNO: 20 },
-    { date: "2024 -04-15", bookNO: 17 },
-    { date: "2024 -04-15", bookNO: 18 },
+    { date: "2024-04-15", bookNO: 10 },
+    { date: "2024-04-15", bookNO: 20 },
+    { date: "2024-04-15", bookNO: 17 },
+    { date: "2024-04-15", bookNO: 18 },
   ];
 
   const filteredDates = bookings
-    .filter((booking) => booking.number > 16)
+    .filter((booking) => booking.bookNO > 16)
     .map((booking) => new Date(booking.date));
 
   return (
-    <div>
+    <div className="mx-auto max-w-lg">
       <DayPicker
-        // selected={startDate}
+        className="w-full border border-gray-200 rounded-md shadow-md p-2"
+        selected={startDate}
         onChange={(date) => {
           setStartDate(date);
         }}
